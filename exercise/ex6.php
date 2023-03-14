35cc0d32b77cc9bed7e5f39ef303de618316135e
phamtung-23 <=>
2023-03-14 14:54:46 +0700

    add all exer1=>6

---
+++ exercise/ex6.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Remove duplicate element</title>
+</head>
+
+<body>
+  <div class="container">
+    <h3 class="text-center text-success">Remove duplicate element</h3>
+    <div class=" w-100 text-center ">
+      <?php
+        function Remove( $arr1, $arr2 ) {
+
+         $different_element1 =  array_diff($arr1, $arr2);
+         $different_element2 =  array_diff($arr2, $arr1);
+
+         $result = array_merge($different_element1,$different_element2);
+         print_r( $result);
+        }
+        Remove([1,2,3,4,5], [1,2,3,4,7,8]);
+      ?>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+  </div>
+</body>
+
+</html>
